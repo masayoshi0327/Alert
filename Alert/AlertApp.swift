fa6036d8b3623fb00fe25b315df862eda05f9be6
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-01 10:10:00 +0900

    Initial Commit

---
+++ Alert/AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertApp.swift
+//  Alert
+//
+//  Created by 澤崎正佳 on 2021/03/01.
+//
+
+import SwiftUI
+
+@main
+struct AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
